049dab201ca5ba08cc5a70476cd5ed358e2ab641
Vinicius <viniciuslima20006@gmail.com>
2026-03-17 09:31:50 -0300

    subindo os primeiros arquivos

---
+++ aula 4/operadores.py
@@ -0,0 +1,19 @@
+# 1.operadores matematicos 
+numero_um =20 
+numero_dois =15
+
+soma = numero_um + numero_dois
+print(F"A soma entre {numero_um} + {numero_dois} = {soma}")
+
+subtração = numero_um - numero_dois
+
+print(F"A subtracao entre {numero_um} - {numero_dois} = {subtração}")
+
+multiplicacão = numero_um * numero_dois
+
+print(F"A multiplicacao entre {numero_um} * {numero_dois} = {multiplicacão}")
+
+divisao =numero_um  / numero_dois
+print(F"A divisao entre {numero_um} / {numero_dois} = {divisao}")
+
+                                       
