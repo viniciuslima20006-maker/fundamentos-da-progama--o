dd3bf68f511f552aba52ec32b2ce48e2519412f9
Vinicius <viniciuslima20006@gmail.com>
2026-04-07 08:31:02 -0300

    subindo arquivo

---
+++ aula_03/aula_03/variaveis.py
@@ -0,0 +1,13 @@
+# variaveis e tipos de dados:
+# Em python voce nao precisa declarar o tipo de uma variavel antes de usa-la. O tipo é inferido automaticamente pelo interpretador.
+# Em python a variável é declarada no padrão snake_case
+# 1. string->
+# 2.number  - int (numero inteiro) ->
+# 3. number - float (numero decimal) ->
+# 4. boolean - True ou False (Verdadeiro ou Falso) ->
+nome = "Gabriel" # string 
+email =  "gabriel@email.com" # string
+cidade = "Rio de Janeiro" # string
+cpf = 15269643766 # number-int 
+salario = 1500.78 # number-float
+casado= False  # boolean
